6fb626ef893f19a394876b31ea95cc09a0b67881
oguzhan-bulbul <70135298+oguzhan-bulbul@users.noreply.github.com>
2022-01-05 23:54:51 +0300

    Initial Project Add

---
+++ src/test/java/com/ouz/springpetclinic/SpringPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouz.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
