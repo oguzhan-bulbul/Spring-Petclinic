63bcbff829003a3b38177c41663c27446a25fca0
oguzhan-bulbul <70135298+oguzhan-bulbul@users.noreply.github.com>
2022-01-13 22:00:22 +0300

    Add-BaseEntity class

--- pet-clinic-data/src/main/java/com/ouz/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/ouz/springpetclinic/services/PetService.java
@@ -5,7 +5,7 @@ import com.ouz.springpetclinic.model.Pet;
 import java.util.Set;
 
 public interface PetService {
-    Pet findById(long id);
+    Pet findById(Long id);
     Pet save(Pet pet);
     Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/ouz/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/ouz/springpetclinic/services/VetService.java
@@ -6,7 +6,7 @@ import com.ouz.springpetclinic.model.Vet;
 import java.util.Set;
 
 public interface VetService {
-    Vet findById(long id);
+    Vet findById(Long id);
     Vet save(Vet vet);
     Set<Vet> findAll();
 }
