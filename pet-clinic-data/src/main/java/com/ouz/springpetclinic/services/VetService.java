11a52ed3d9f9e69645154f43d27ed8217f8662e6
oguzhan-bulbul <70135298+oguzhan-bulbul@users.noreply.github.com>
2022-01-16 20:32:59 +0300

    Refactor Services to Common Interface. closes #11

--- pet-clinic-data/src/main/java/com/ouz/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/ouz/springpetclinic/services/VetService.java
@@ -5,8 +5,5 @@ import com.ouz.springpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long>{
 }
