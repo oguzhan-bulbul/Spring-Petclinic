bcaa3c2c48d8c1aafa9c46466e958dcb9cfd134f
oguzhan-bulbul <70135298+oguzhan-bulbul@users.noreply.github.com>
2022-02-26 23:33:47 +0300

    Added visit , updated owner with pet relationship . Closes #26

--- pet-clinic-data/src/main/java/com/ouz/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/ouz/springpetclinic/services/OwnerService.java
@@ -2,7 +2,6 @@ package com.ouz.springpetclinic.services;
 
 import com.ouz.springpetclinic.model.Owner;
 
-import java.util.Set;
 
 public interface OwnerService extends CrudService<Owner,Long>{
     Owner findByLastName(String lastName);
--- pet-clinic-data/src/main/java/com/ouz/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/ouz/springpetclinic/services/PetService.java
@@ -2,7 +2,6 @@ package com.ouz.springpetclinic.services;
 
 import com.ouz.springpetclinic.model.Pet;
 
-import java.util.Set;
 
 public interface PetService extends CrudService<Pet,Long>{
 }
