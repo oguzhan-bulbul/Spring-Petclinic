f4238baffb1c5e50d4674f090c9e761995ca012f
oguzhan-bulbul <70135298+oguzhan-bulbul@users.noreply.github.com>
2022-02-27 23:21:23 +0300

    Added PetTypes to DataLoader. Closes #32

--- pet-clinic-web/src/main/java/com/ouz/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/ouz/springpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,11 @@
 package com.ouz.springpetclinic.bootstrap;
 
 import com.ouz.springpetclinic.model.Owner;
+import com.ouz.springpetclinic.model.PetType;
 import com.ouz.springpetclinic.model.Vet;
 import com.ouz.springpetclinic.services.OwnerService;
+import com.ouz.springpetclinic.services.PetTypeService;
 import com.ouz.springpetclinic.services.VetService;
-import com.ouz.springpetclinic.services.map.OwnerServiceMap;
-import com.ouz.springpetclinic.services.map.VetServiceMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,14 +15,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+		this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+    	
+    	PetType dog = new PetType();
+    	dog.setName("Dog John");
+    	PetType savedDogPetType = petTypeService.save(dog);
+    	
+    	PetType cat = new PetType();
+    	cat.setName("Cat John");
+    	PetType savedCatPetType = petTypeService.save(cat);
+    	
         Owner owner1 =  new Owner();
         owner1.setFirstName("Oguzhan");
         owner1.setLastName("BULBUL");
